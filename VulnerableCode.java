0bcec6c6ec3bde0d676c5da57ba18141b10bd376
jayarajcl <125452567+jayarajcl@users.noreply.github.com>
2024-12-03 14:02:14 +0530

    outputCode.java

--- VulnerableCode.java
+++ VulnerableCode.java
@@ -1,30 +1,36 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Open Redirect Form</title>
-</head>
-<body>
-    <h2>Redirect to External URL</h2>
-    <form action="RedirectServlet" method="get">
-        <label for="url">URL:</label>
-        <input type="text" id="url" name="url"><br><br>
-        <input type="submit" value="Redirect">
-    </form>
-</body>
-</html>
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+public class SecureCode {
 
-public class RedirectServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
-        String url = request.getParameter("url");
-        try {
-            // Vulnerable redirect logic without validation
-            response.sendRedirect(url);
-        } catch (Exception e) {
-            e.printStackTrace();
+public static void main(String[] args) {
+    String dbUrl = "jdbc:mysql://localhost:3306/mydb";
+    String username = "admin";
+    String password = "password123";
+
+    try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Enter username:");
+        String userInput = scanner.nextLine();
+
+        String query = "SELECT * FROM users WHERE username = ?";
+
+        Connection connection = DriverManager.getConnection(dbUrl, username, password);
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, userInput);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            System.out.println("Welcome, " + resultSet.getString("username") + "!");
+        } else {
+            System.out.println("User not found.");
         }
+
+    } catch (Exception e) {
+        e.printStackTrace();
     }
+}
 }
