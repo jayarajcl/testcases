7cf0a3e7ebbf8589d7627abaae7abbcae0e6908d
jayarajcl <125452567+jayarajcl@users.noreply.github.com>
2025-01-16 16:59:05 +0530

    Update 201.cs

--- 201.cs
+++ 201.cs
@@ -1,14 +1,8 @@
-public class Handler : IHttpHandler
-{
-    public void ProcessRequest(HttpContext ctx)
-    {
-        try
-        {
-            ...
-        }
-        catch (AuthenticationFailure ex)
-        {
-            ctx.Response.Write("Invalid credentials provided.");
-        }
-    }
+public class Handler : IHttpHandler{    public void ProcessRequest(HttpContext ctx)    {     
+   try        {  
+     ...    }   
+     catch (AuthenticationFailure ex)      
+  {     
+       ctx.Response.Write("Invalid password: " + password);        } 
+   }
 }
