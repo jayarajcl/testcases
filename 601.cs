c603efcce690492d358afa56f1180c109c46a492
jayarajcl <125452567+jayarajcl@users.noreply.github.com>
2025-01-16 17:01:41 +0530

    Update 601.cs

--- 601.cs
+++ 601.cs
@@ -1,16 +1,11 @@
 using System;
 using System.Web;
-using System.Collections.Generic;
 
 public class UnvalidatedUrlHandler : IHttpHandler
 {
-    private List<string> VALID_REDIRECTS = new List<string>{};
-
     public void ProcessRequest(HttpContext ctx)
     {
-        if (VALID_REDIRECTS.Contains(ctx.Request.QueryString["page"]))
-        {
-            ctx.Response.Redirect(ctx.Request.QueryString["page"]);
-        }
+        // BAD: a request parameter is incorporated without validation into a URL redirect
+        ctx.Response.Redirect(ctx.Request.QueryString["page"]);
     }
 }
